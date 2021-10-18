da7144d4a9712d90580b3729403221cd0879ca64
JoeBro218 <70218539+JoeBro218@users.noreply.github.com>
2021-10-18 15:14:19 -0400

    Now time for the project.

--- Zork.Common/Room.cs
+++ Zork.Common/Room.cs
@@ -45,5 +45,11 @@ namespace Zork
         public override int GetHashCode() => Name.GetHashCode();
 
         public void UpdateNeighbors(World world) => Neighbors = (from entry in NeighborNames let room = world.RoomsByName.GetValueOrDefault(entry.Value) where room != null select (Direction: entry.Key, Room: room)).ToDictionary(pair => pair.Direction, pair => pair.Room);
+    
+        //public void UpdateNeighbors(World world)
+        //{
+        //    Neighbors = new Dictionary<>
+        //    foreach
+        //}
     }
 }
